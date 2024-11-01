d04ba1d4a6dda0fdc8c0ae109bfb64bf1cdf9894
Marcus Vinicius Dos Santos Gomes <marcudos@c2r4p4.42sp.org.br>
2024-11-01 19:34:47 -0300

    Started - only files created

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcudos <marcudos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 19:18:15 by marcudos          #+#    #+#             */
+/*   Updated: 2024/11/01 19:32:36 by marcudos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+
+#endif
